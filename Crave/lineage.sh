eb274d7e8f62b96c3b4489c9874abce41a868287
mayuresh <mayureshnanal846@gmail.com>
2025-06-30 15:08:07 +0530

    initial skeleton for scripts

---
+++ Crave/lineage.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b lineage-15 https://github.com/Mayuresh2543/local_manifests.git .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+rm -rf packages/apps/Updater
+git clone https://github.com/Mayuresh2543/lineage_packages_apps_Updater.git packages/apps/Updater
+rm -rf packages/apps/Trebuchet
+git clone https://github.com/Mayuresh2543/lineage_packages_apps_Trebuchet.git packages/apps/Trebuchet
+echo "Custom sources synced"
+
+# Export
+export BUILD_USERNAME=mayuresh
+export BUILD_HOSTNAME=crave
+export TZ="Asia/India"
+echo "======= Export Done ======"
+
+# Set up build environment
+. build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_stone-bp1a-userdebug
+echo "============="
+
+# Build rom
+m bacon
