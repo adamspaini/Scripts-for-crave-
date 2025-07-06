b9164ee1078bfbe18ed5714266e91454c7605dc3
Mayuresh <92848611+mayuresh2543@users.noreply.github.com>
2025-07-06 12:34:24 +0530

    test

--- Crave/yaap.sh
+++ Crave/yaap.sh
@@ -18,6 +18,15 @@ echo "============================"
 /opt/crave/resync.sh
 echo "============================"
 
+export CLANG_PATH=$PWD/prebuilts/clang/host/linux-x86/clang-r547379/bin
+export PATH=$CLANG_PATH:$PATH
+alias clang=$CLANG_PATH/clang
+alias clang++=$CLANG_PATH/clang++
+alias ld.lld=$CLANG_PATH/ld.lld
+export CC=$CLANG_PATH/clang
+export CXX=$CLANG_PATH/clang++
+export LD=$CLANG_PATH/ld.lld
+
 # Export
 export BUILD_USERNAME=mayuresh
 export BUILD_HOSTNAME=crave
