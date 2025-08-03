c2b13cabb49fba8267cbadcdf5709f264508d451
mayuresh <mayureshnanal846@gmail.com>
2025-08-03 22:35:26 +0530

    infinity: switch to prebuilt custom kernel

--- Crave/infinity.sh
+++ Crave/infinity.sh
@@ -22,7 +22,7 @@ rm -rf prebuilts/calyx/datura
 # Clone Device sources
 git clone https://github.com/mayuresh2543/device_xiaomi_stone_new.git -b infinity-16 --depth=1 device/xiaomi/stone
 git clone https://github.com/mayuresh2543/vendor_xiaomi_stone.git -b 16 --depth=1 vendor/xiaomi/stone
-git clone https://github.com/mayuresh2543/kernel_xiaomi_stone.git -b 16 --depth=1 kernel/xiaomi/stone
+git clone https://github.com/mayuresh2543/device_xiaomi_stone-kernel.git -b 16 --depth=1 device/xiaomi/stone-kernel
 git clone https://github.com/mayuresh2543/hardware_dolby.git -b sony-1.0 --depth=1 hardware/dolby
 git clone https://github.com/mayuresh2543/vendor_bcr.git --depth=1 vendor/bcr
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 --depth=1 hardware/xiaomi
@@ -55,4 +55,4 @@ lunch infinity_stone-userdebug
 echo "============="
 
 # Build rom
-mka bacon
+m bacon
