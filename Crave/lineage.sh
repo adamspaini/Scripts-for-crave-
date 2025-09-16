d11b1d26b12a7d3b290ac52c8ddabca777def773
Mayuresh <92848611+mayuresh2543@users.noreply.github.com>
2025-09-16 07:47:32 +0530

    Update lineage.sh

--- Crave/lineage.sh
+++ Crave/lineage.sh
@@ -4,7 +4,7 @@ rm -rf .repo/local_manifests/
 rm -rf prebuilts/clang/host/linux-x86
 
 # Rom source repo
-repo init --git-lfs --no-clone-bundle -u https://git@github.com/LineageOS/android.git -b refs/changes/42/436442/28
+repo init --git-lfs --no-clone-bundle -u https://git@github.com/LineageOS/android.git -b refs/changes/42/436442/31
 echo "=================="
 echo "Repo init success"
 echo "=================="
