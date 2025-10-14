35bff217128fa570df4e33d1a9212e8d9a72e915
Mayuresh <92848611+mayuresh2543@users.noreply.github.com>
2025-10-14 10:14:03 +0530

    Update lineage.sh

--- Crave/lineage.sh
+++ Crave/lineage.sh
@@ -19,8 +19,8 @@ echo "============================"
 /opt/crave/resync.sh
 echo "============================"
 
-#rm -rf vendor/lineage
-#git clone https://github.com/mayuresh2543/android_vendor_lineage.git --depth=1 vendor/lineage
+rm -rf vendor/lineage
+git clone https://github.com/mayuresh2543/android_vendor_lineage.git --depth=1 vendor/lineage
 
 rm -rf packages/apps/Updater
 git clone https://github.com/mayuresh-releases/lineage_packages_apps_Updater.git --depth=1 packages/apps/Updater
