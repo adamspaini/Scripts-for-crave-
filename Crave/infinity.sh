24bf3624e44e70c69821c369dbdaa5be414451e2
Mayuresh <92848611+mayuresh2543@users.noreply.github.com>
2025-11-07 22:28:17 +0530

    Update infinity.sh

--- Crave/infinity.sh
+++ Crave/infinity.sh
@@ -18,7 +18,7 @@ rm -rf packages/apps/ViPER4AndroidFX
 
 # Clone Device sources
 git clone https://github.com/mayuresh2543/device_xiaomi_stone_rebase.git -b 16-infinity --depth=1 device/xiaomi/stone
-git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_stone.git -b 16 --depth=1 vendor/xiaomi/stone
+git clone https://github.com/mayuresh2543/vendor_xiaomi_stone.git -b 16 --depth=1 vendor/xiaomi/stone
 git clone https://github.com/mayuresh2543/kernel_xiaomi_stone_rebase.git -b 16 --depth=1 kernel/xiaomi/stone
 git clone https://github.com/mayuresh2543/hardware_dolby.git -b sony-1.0 --depth=1 hardware/dolby
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 --depth=1 hardware/xiaomi
