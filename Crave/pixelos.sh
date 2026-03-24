24a1a9342a8b8c9826f67e2513bca8d283d5f956
Prabhuswar Saha <prabhuswarppsaha@gmail.com>
2026-03-24 11:41:34 +0530

    Update and rename lineage.sh to pixelos.sh

---
+++ Crave/pixelos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b lineage-16 https://github.com/prabhu992/local_manifests.git .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Export
+export BUILD_USERNAME=mayuresh
+export BUILD_HOSTNAME=crave
+export TZ="Asia/India"
+echo "======= Export Done ======"
+
+# Set up build environment
+. build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+breakfast stone
+echo "============="
+
+# Install clean
+m installclean
+
+# Build rom
+m pixelos
